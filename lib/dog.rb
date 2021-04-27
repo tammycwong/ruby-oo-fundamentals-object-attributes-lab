faf28e15e3ca97cac7aed56cb26cb22d6e13eef2
Tammy <tammycwong@gmail.com>
2021-04-27 16:02:25 -0400

    finished

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def breed=(new_breed)
+        @breed = new_breed
+    end
+    
+    def breed
+        @breed 
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+end
